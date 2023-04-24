89bf1fcde5c552d77e69984409e5c2b8337fc49e
bahasaracoglu <bahasaracoglu@gmail.com>
2023-04-24 16:28:34 +0300

    Daha iyi bir hazır action bağlandı.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,11 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
-export const addOne = (e) => {
-  console.log(e);
+export const addOne = () => {
   return { type: ADD_ONE };
 };
 
 export const applyNumber = (number) => {
+  console.log(number);
   return { type: APPLY_NUMBER, payload: number };
 };
